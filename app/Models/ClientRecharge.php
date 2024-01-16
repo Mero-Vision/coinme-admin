e2cf4758faa29c641511394f87e07368abe85672
Hancie123 <hanciewanemphago@gmail.com>
2024-01-16 23:14:57 +0545

    freeze added

---
+++ app/Models/ClientRecharge.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientRecharge extends BaseModel
+{
+    use HasFactory;
+}
