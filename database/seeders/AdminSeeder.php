f6584f002a4e0ef017b2efac48bb46e62f4ffd0b
Hancie123 <hanciewanemphago@gmail.com>
2024-02-05 12:30:54 +0545

    validation updated

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,19 +15,47 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        $userData = [
-            'name' => 'Reyan Shrestha',
-            'email' => 'reyanshrestha@gmail.com',
+        $admin1data = [
+            'name' => 'Admin One',
+            'email' => 'will4u2love@gmail.com',
             'gender'=>'Male',
-            'password' => Hash::make('password'),
-            'address' => 'Kathmandu',
-            'mobile_no' => '9825915122',
+            'password' => Hash::make('Password'),
+            'address' => 'US',
+            'mobile_no' => '+5654475',
             'email_verified_at' => Carbon::now(),
             'status'=>'admin',
 
         ];
 
-        $user = User::firstOrCreate(['email' => $userData['email']], $userData);
+        $user1 = User::firstOrCreate(['email' => $admin1data['email']], $admin1data);
+
+        $admin2data = [
+            'name' => 'Admin Two',
+            'email' => 'birukhadkachetri@gmail.com',
+            'gender' => 'Male',
+            'password' => Hash::make('Password'),
+            'address' => 'US',
+            'mobile_no' => '+5654465',
+            'email_verified_at' => Carbon::now(),
+            'status' => 'admin',
+
+        ];
+
+        $user2 = User::firstOrCreate(['email' => $admin2data['email']], $admin2data);
+
+        $admin3data = [
+            'name' => 'Admin Three',
+            'email' => 'tetherxchange@gmail.com',
+            'gender' => 'Male',
+            'password' => Hash::make('Password'),
+            'address' => 'US',
+            'mobile_no' => '+5654465',
+            'email_verified_at' => Carbon::now(),
+            'status' => 'admin',
+
+        ];
+
+        $user3 = User::firstOrCreate(['email' => $admin3data['email']], $admin3data);
 
     }
 }
