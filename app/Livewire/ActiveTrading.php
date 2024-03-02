ac0d593d6290526dfbd24abbc1803bdbe6313378
Hancie123 <hanciewanemphago@gmail.com>
2024-03-02 13:49:24 +0545

    active trading added

--- app/Livewire/ActiveTrading.php
+++ app/Livewire/ActiveTrading.php
@@ -14,7 +14,8 @@ class ActiveTrading extends Component
     public function render()
     {
         $this->trades = TradeTransaction::join('users', 'users.id','=', 'trade_transactions.client_id')
-        ->where('trade_transactions.trade_status','!=', null)->latest('trade_transactions.created_at','desc')->get();
+        ->select('users.name', 'trade_transactions.trade_type', 'trade_transactions.purchase_amount', 'trade_transactions.purchase_price', 'trade_transactions.created_at')
+        ->where('trade_transactions.trade_status', null)->latest('trade_transactions.created_at','desc')->get();
 
             
         return view('livewire.active-trading');
